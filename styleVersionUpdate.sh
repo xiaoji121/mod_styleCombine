64442ff45cb93e63bb8c87212eb22b9e15c56d31
zhiwen <doudouhamao@gmail.com>
2012-05-23 14:50:05 +0800

    shell opz

--- styleVersionUpdate.sh
+++ styleVersionUpdate.sh
@@ -5,30 +5,34 @@ STYLE_DOMAIN="http://172.22.9.69"
 DOWNLOAD_DIR="${forum_output}"
 STYLE_VERSION_URI="styleVersion" 
 
-#default 10 minutes
-interval="5m"
+#default 2 minutes
+interval="2m"
 
 lastModified=""
 httpStatus=0
 
 STYLE_VERSION_URL="${STYLE_DOMAIN}/${STYLE_VERSION_URI}.gz" 
 DOWNLOAD_DIR_FILE="${DOWNLOAD_DIR}/${STYLE_VERSION_URI}.gz"
+DOWNLOAD_DIR_FILE_TMP="${DOWNLOAD_DIR}/${STYLE_VERSION_URI}.gz_tmp"
 UNGZIP_DIR_FILE="${DOWNLOAD_DIR}/${STYLE_VERSION_URI}"
+RESPONSE_LOG="${DOWNLOAD_DIR}/response"
 
 getLastModified() {
-    if [ -f "$1" ]
-        then lastModified=`cat $1 |grep "Last-Modified:" | awk -F"Last-Modified:" '{print $2}'`
-        else lastModified=""
+	responseLog=$1
+	lastModified=""
+    if [ -f "$responseLog" ]; then
+        lastModified=`cat $responseLog |grep "Last-Modified:" | awk -F"Last-Modified:" '{print $2}'`
     fi
 }
 
 getHttpStatus() {
-    if [ -f "$1" ]
-        then httpStatus=`cat $1 |grep "HTTP/" | awk '{print $2}'`
-        	if [ -z "$httpStatus" ]; then
-        		httpStatus=0
-        	fi
-        else httpStatus=0
+	responseLog=$1
+	httpStatus=0
+    if [ -f "$responseLog" ]; then
+        httpStatus=`cat $responseLog |grep "HTTP/" | awk '{print $2}'`
+        if [ -z "$httpStatus" ]; then
+        	httpStatus=0
+        fi
     fi
 }
 
@@ -36,34 +40,55 @@ ungzip() {
     `$gzip -cd $1 > $2`
 }
 
-download() {
+checkAndUngzip() {
+	
+	getHttpStatus $RESPONSE_LOG
+        
+    ##如果返回的内容是html内容则表示出错，不将版本文件替换；如果下载成功grep 二进制文件则会报错，所以添加-v去除错误
+    isHtml=`cat ${DOWNLOAD_DIR_FILE_TMP} | head -10| grep "<" | grep -v "Binary file"`
 
-    getLastModified $4
-
-    if [ -z "$lastModified" ]; then
-        wget -S -t 0 -T 5 $1 -O $2 > $4 2>&1
-        ungzip $2 $3
-    else wget -S -t 0 -T 5 --header="If-Modified-Since:$lastModified" $1 -O ${2}_t > $4 2>&1
-        getHttpStatus $4
-        if [ $httpStatus -eq 200 ]; then
-            mv ${2}_t $2
-            ungzip $2 $3
-        fi
+	##http状态返回200表示正确，如果是304或其它则不做任何处理    
+    if [ $httpStatus -eq 200 ] && [ -z "$isHtml" ]; then
+    	## 将tmp文件修改成可用文件
+        mv ${DOWNLOAD_DIR_FILE_TMP} ${DOWNLOAD_DIR_FILE}
+        
+        ##将文件减压到 ${UNGZIP_DIR_FILE} 目录文件中
+        ungzip ${DOWNLOAD_DIR_FILE} ${UNGZIP_DIR_FILE}
     fi
+}
+
+download() {
+	
+	getHttpStatus $RESPONSE_LOG
+	##只要不是304才去读取新的lastModified,否则lastModified永远不会被修改掉
+	if [ $httpStatus -ne 304 ]; then
+		getLastModified $RESPONSE_LOG	
+	fi
+    
+	##如果有lastModified信息，则添加头信息去请求
+	param=""
+    if [ -n "$lastModified" ]; then
+    	param="--header=If-Modified-Since:${lastModified}"
+    fi
+    ##下载版本文件
+    wget -S -t 0 -T 5 "$param" ${STYLE_VERSION_URL} -O ${DOWNLOAD_DIR_FILE_TMP} > $RESPONSE_LOG 2>&1
+    checkAndUngzip
 }
 
 main() {
     mkdir -p $DOWNLOAD_DIR
     if [ -f "${UNGZIP_DIR_FILE}" ]; then
-    	rm ${UNGZIP_DIR_FILE}
+    	rm ${UNGZIP_DIR_FILE}*
     fi
-    if [ -f "${DOWNLOAD_DIR}/response" ]; then
-    	rm ${DOWNLOAD_DIR}/response
+    if [ -f "$RESPONSE_LOG" ]; then
+    	rm $RESPONSE_LOG
     fi
     
     while [ true ]; do
-        download "${STYLE_VERSION_URL}" "${DOWNLOAD_DIR_FILE}" "${UNGZIP_DIR_FILE}" "${DOWNLOAD_DIR}/response"
+        ##下载调用
+        download
         sleep $interval
     done
 }
+
 main
